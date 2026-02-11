2e08ca06d0bb8cf0a4172dd16f5050dfc39088c8
Prashun <prashunsharma1909@gmail.com>
2026-02-11 14:30:45 +0530

    new commit

--- services/shared/main.py
+++ services/shared/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
